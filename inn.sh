c1bde266bedcf02e26e4c8b05f9fbda2c2baf749
Abba Yosua <37859571+abbayosua@users.noreply.github.com>
2024-02-20 23:28:17 +0700

    Create inn.sh

---
+++ inn.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo add-apt-repository ppa:otter-browser/release
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install v4l2loopback-dkms
+sudo apt install otter-browser
+sudo apt install obs-studio
